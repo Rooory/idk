53b7c3ccab377366032e6cdb2d45104993f019a9
Student   COVENTRY Rory <05452@farnborough.ac.uk>
2020-11-06 11:00:45 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        String whello = "Hello";
+        System.out.println(whello);
+        System.out.println(whello.length());
+        }
+        }
+
